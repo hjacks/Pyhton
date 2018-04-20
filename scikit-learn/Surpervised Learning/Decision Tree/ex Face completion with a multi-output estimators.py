12360a80f9f3ab98dac16afcd5b1875515059c0c
hjacks <hjacks@163.com>
2018-04-20 18:15:12 +0800

    add

---
+++ scikit-learn/Surpervised Learning/Decision Tree/ex Face completion with a multi-output estimators.py
@@ -0,0 +1,10 @@
+"""
+This example shows the use of multi-output
+estimator to complete images. The goal is to
+predict the lower half of a face given its upper
+ half.
+"""
+
+print(__doc__)
+
+import
